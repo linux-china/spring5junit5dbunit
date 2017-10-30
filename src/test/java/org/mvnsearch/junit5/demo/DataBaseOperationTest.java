3fd938231831e6e964e6dedae455df9eb2f9e259
linux_china <linux_china@hotmail.com>
2017-10-30 23:03:21 +0800

    add generate DTD

--- src/test/java/org/mvnsearch/junit5/demo/DataBaseOperationTest.java
+++ src/test/java/org/mvnsearch/junit5/demo/DataBaseOperationTest.java
@@ -1,13 +1,18 @@
 package org.mvnsearch.junit5.demo;
 
 
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.xml.FlatDtdDataSet;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mvnsearch.junit5.DataSet;
 import org.mvnsearch.junit5.DBUnitExtension;
+import org.mvnsearch.junit5.DataSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.io.FileOutputStream;
 
 /**
  * Unit test for database
@@ -26,5 +31,11 @@ public class DataBaseOperationTest {
         jdbcTemplate.query("SELECT id, nick FROM people", (resultSet, i) -> {
             return resultSet.getString("nick");
         });
+    }
+
+    //@Test
+    public void generateDTD() throws Exception {
+        IDatabaseConnection connection = new DatabaseConnection(jdbcTemplate.getDataSource().getConnection());
+        FlatDtdDataSet.write(connection.createDataSet(), new FileOutputStream("lazada-address-server/src/test/resource/dataset/lazada-address.dtd"));
     }
 }
